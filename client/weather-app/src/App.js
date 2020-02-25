60daed83b5db55fb40d5199da2b66982a1def674
MicahEhrlich <unreal3.ehrlich@gmail.com>
2020-02-25 19:22:46 +0200

    Basic React Client

--- client/weather-app/src/App.js
+++ client/weather-app/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
+import Weather from './components/Weather';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-    
+    <div className='App'>
+      <header className='App-header'>
+        
       </header>
+      <Weather />
     </div>
   );
 }
---
+++ client/weather-app/src/components/Weather.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1
+  },
+  paper: {
+    height: 140,
+    width: 100
+  },
+  control: {
+    padding: theme.spacing(2)
+  }
+}));
+
+const Weather = () => {
+  const classes = useStyles();
+
+  const getWeather = () => {
+    
+  }
+
+  const getWeatherDetails = async city => {
+
+  }
+
+  return (
+    <div>
+      <Grid container className={classes.root} spacing={2}>
+        <Grid item xs={12}>
+          <Grid container justify='center' spacing={10}>
+            {[0, 1, 2].map(value => (
+              <Grid key={value} item>
+                <Paper className={classes.paper} />
+              </Grid>
+            ))}
+          </Grid>
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
+
+export default Weather;
